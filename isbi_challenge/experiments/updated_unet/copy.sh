85bb7e0d5c2a899043439d18d757660b960d1395
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-10-29 20:59:34 +0300

    updated model

---
+++ isbi_challenge/experiments/updated_unet/copy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gcloud compute scp --recurse --zone "us-central1-a" --project "electric-autumn-245617" \
+ilyarudyak@instance-2-gpu:~/cs230_project_keras/isbi_challenge/experiments/updated_unet/ev* .
