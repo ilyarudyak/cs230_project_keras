5931889ea6868f2249b0bda9848e568d8c6e1827
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-12-15 15:28:58 +0300

    done VGG transfer learning (1st dradft)

--- skin_lesion/train.py
+++ skin_lesion/train.py
@@ -225,4 +225,4 @@ if __name__ == '__main__':
                   experiment_dir=experiment_dir,
                   is_toy=True,
                   set_seed=True)
-    tuner.tune_lr(rates=(1e-1, 1e-2, 1e-3, 1e-4, 1e-5))
+    tuner.tune_lr(rates=(1e-4, 1e-5, 1e-6))
