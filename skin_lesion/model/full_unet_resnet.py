f27ab70dcf8b458171c41c8efdfbcd28fc3a9c02
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-12-17 00:44:42 +0300

    done resnet encoder (1st draft)

--- skin_lesion/model/full_unet_resnet.py
+++ skin_lesion/model/full_unet_resnet.py
@@ -4,9 +4,8 @@ from utils import Params
 
 class FullUnetResnet:
     """
-    This model has the shape specified in the original paper,
-    uses tf.keras.layers.UpSampling2D (not tf.keras.layers.Conv2DTranspose) and relu activation
-    (not leaky relu). This model uses MaxPool, not BatchNorm.
+    build upon:
+    https://github.com/killthekitten/kaggle-carvana-2017/blob/master/models.py
     """
 
     def __init__(self,
@@ -24,7 +23,17 @@ class FullUnetResnet:
                                                          input_shape=params.input_shape)
         self.vgg_model = tf.keras.applications.VGG16(weights='imagenet',
                                                      include_top=False,
+                                                     input_tensor=self.base_model.input,
                                                      input_shape=params.input_shape)
+
+        # naming convention is changed in tf.keras
+        self.resnet_config = {
+            'activation_1': 'conv1_relu',
+            'activation_10': 'conv2_block3_out',
+            'activation_22': 'conv3_block4_out',
+            'activation_40': 'conv4_block6_out',
+            'activation_49': 'conv5_block3_out'
+        }
 
         if freeze:
             self.freeze_layers()
@@ -49,58 +58,46 @@ class FullUnetResnet:
         if self.set_seed:
             tf.random.set_seed(self.params.seed)
 
-        x = self.base_model.output
+        conv1 = self.base_model.get_layer(self.resnet_config['activation_1']).output
+        conv2 = self.base_model.get_layer(self.resnet_config['activation_10']).output
+        conv3 = self.base_model.get_layer(self.resnet_config['activation_22']).output
+        conv4 = self.base_model.get_layer(self.resnet_config['activation_40']).output
+        conv5 = self.base_model.get_layer(self.resnet_config['activation_49']).output
 
-        bottom = self.double_block(x, 1024, prefix='bottom')
+        up6 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D()(conv5), conv4], axis=-1)
+        conv6 = self.conv_block_simple(up6, 256, "conv6_1")
+        conv6 = self.conv_block_simple(conv6, 256, "conv6_2")
 
-        # 14 x 14 block5
-        left_block5 = self.base_model.get_layer('conv4_block6_out').output
-        right_conv5 = self.up_sampling_block(bottom, left_block5, 512, 'right_conv5')
+        up7 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D()(conv6), conv3], axis=-1)
+        conv7 = self.conv_block_simple(up7, 192, "conv7_1")
+        conv7 = self.conv_block_simple(conv7, 192, "conv7_2")
 
-        # 28 x 28 block4
-        left_block4 = self.base_model.get_layer('conv3_block4_out').output
-        right_conv4 = self.up_sampling_block(right_conv5, left_block4, 256, 'right_conv4')
+        up8 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D()(conv7), conv2], axis=-1)
+        conv8 = self.conv_block_simple(up8, 128, "conv8_1")
+        conv8 = self.conv_block_simple(conv8, 128, "conv8_2")
 
-        # 56 x 56 block3
-        left_block3 = self.base_model.get_layer('conv2_block3_out').output
-        right_conv3 = self.up_sampling_block(right_conv4, left_block3, 128, 'right_conv3')
+        up9 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D()(conv8), conv1], axis=-1)
+        conv9 = self.conv_block_simple(up9, 64, "conv9_1")
+        conv9 = self.conv_block_simple(conv9, 64, "conv9_2")
 
-        # 112 x 112 block2
-        left_block2 = self.base_model.get_layer('conv1_relu').output
-        right_conv2 = self.up_sampling_block(right_conv3, left_block2, 64, 'right_conv2')
-
-        # Resnet doesn't have 224 x 224 layer conv, use first conv layer in VGG16 instead
         vgg_first_conv = self.vgg_model.get_layer("block1_conv2").output
-        right_conv1 = self.up_sampling_block(right_conv2, vgg_first_conv, 32, 'right_conv1')
-        right_dropout1 = tf.keras.layers.SpatialDropout2D(0.2)(right_conv1)
+        up10 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D()(conv9), self.base_model.input, vgg_first_conv], axis=-1)
+        conv10 = self.conv_block_simple(up10, 32, "conv10_1")
+        conv10 = self.conv_block_simple(conv10, 32, "conv10_2")
+        conv10 = tf.keras.layers.SpatialDropout2D(0.2)(conv10)
 
-        # Sigmoid
-        output = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(right_dropout1)
+        output = tf.keras.layers.Conv2D(1, (1, 1), activation="sigmoid", name="prediction")(conv10)
+
         self.model = tf.keras.models.Model(inputs=self.base_model.input, outputs=output)
 
-    def conv_block(self, prevlayer, filters, prefix, strides=(1, 1)):
+    def conv_block_simple(self, prev_layer, filters, prefix, strides=(1, 1)):
         conv = tf.keras.layers.Conv2D(filters, (3, 3),
                                       padding="same",
-                                      kernel_initializer="he_normal",
+                                      kernel_initializer=self.kernel_initializer,
                                       strides=strides,
-                                      name=prefix + "_conv")(prevlayer)
-        # if batch_norm:
-        #     conv = BatchNormalization(name=prefix + "_bn")(conv)
-        # if layer_norm:
-        #     conv = LayerNormalization()(conv)
-        conv = tf.keras.layers.LeakyReLU(alpha=0.001,
-                                         name=prefix + "_activation")(conv)
+                                      name=prefix + "_conv")(prev_layer)
+        conv = tf.keras.layers.LeakyReLU(alpha=self.alpha, name=prefix + "_activation")(conv)
         return conv
-
-    def double_block(self, prevlayer, filters, prefix, strides=(1, 1)):
-        layer1 = self.conv_block(prevlayer, filters, prefix + "1", strides)
-        layer2 = self.conv_block(layer1, filters, prefix + "2", strides)
-        return layer2
-
-    def up_sampling_block(self, up_sampling_layer, left_skip_layer, filters, prefix, strides=(1, 1)):
-        up_layer = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D(size=(2, 2))(up_sampling_layer), left_skip_layer], axis=3)
-        double_block_layer = self.double_block(up_layer, filters, prefix, strides)
-        return double_block_layer
 
 
 if __name__ == '__main__':
